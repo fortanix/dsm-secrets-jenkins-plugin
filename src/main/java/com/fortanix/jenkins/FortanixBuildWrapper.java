371df9cd293a06f8f709c28cc4fc318fee91aa20
arushi.pahuja@fortanix.com <arushi.pahuja@fortanix.com>
2023-07-10 12:34:55 +0530

    Fixed Credential Deselected in Project Config

--- src/main/java/com/fortanix/jenkins/FortanixBuildWrapper.java
+++ src/main/java/com/fortanix/jenkins/FortanixBuildWrapper.java
@@ -96,7 +96,7 @@ public class FortanixBuildWrapper extends SimpleBuildWrapper {
 
 
     private FortanixCredentials getCredentials(Run build) {
-        String id = getConfiguration().getFortanixCredentialId();
+        String id = getConfiguration().getftxCredentialId();
         LOGGER.log(Level.INFO, "Get Credentials ID: "+ id);
         if (StringUtils.isBlank(id)) {
             throw new RuntimeException("The credential id was not configured - please specify the credentials to use.");
